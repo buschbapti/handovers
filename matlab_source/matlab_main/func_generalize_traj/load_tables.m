cd7ce1c8a263043421ddb25e8c5beb6c91fe5724
Guilherme Maeda <gjmaeda@gmail.com>
2015-11-30 15:48:03 +0100

    Baxter elbow up trajectories.

--- matlab_source/matlab_main/func_generalize_traj/load_tables.m
+++ matlab_source/matlab_main/func_generalize_traj/load_tables.m
@@ -3,21 +3,22 @@ function lookupTraj = load_tables(folderName)
     folder = ['./' folderName];
         
     if strcmp(folderName, 'lookupTraj1')
-        load([folder '/table1_20151111_113045.mat']);
+        %load([folder '/table1_20151111_113045.mat']);
+        load([folder '/table1_20151130_121146.mat']);        
         lookupTraj{1} = solTraj1;
         
-        load([folder '/del1_20151112_104318.mat']);
-        lookupTraj{end+1} = solTraj1;       
+        %load([folder '/del1_20151112_104318.mat']);
+        %lookupTraj{end+1} = solTraj1;       
     end
 
     if strcmp(folderName, 'lookupTraj2')
         
-        
-        load([folder '/table2_20151111_113045.mat']);
+        %load([folder '/table2_20151111_113045.mat']);
+        load([folder '/table2_20151130_115714.mat']);
         lookupTraj{1} = solTraj2;
         
-        load([folder '/del2_20151112_104318.mat']);
-        lookupTraj{end+1} = solTraj2;
+        %load([folder '/del2_20151112_104318.mat']);
+        %lookupTraj{end+1} = solTraj2;
         
     end
 
