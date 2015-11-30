373a2fea731e07d4b792ca0a1c3c6826b02ad84d
buschbapti <baptiste.busch@inria.fr>
2015-11-30 17:05:49 +0100

    Correct bugs when folder were not empty

--- matlab_source/matlab_main/main.m
+++ matlab_source/matlab_main/main.m
@@ -31,7 +31,7 @@ drawnow;
 % If this flag is 1 no ROS node is required and poses are generated
 % randomly. If the flag is zero, it will wait for a real pose in 
 % posesFromROS.txt and for the flag  flagROSfinished.txt
-run_without_ROS_trigger = 1;
+run_without_ROS_trigger = 0;
 
 paramGeneral.initialDT = 0.5;  % seconds to wait at the first trajectory state, such that Baxter does not jump.
 paramGeneral.offsetGripper_humanHand = 0.05 ; % in meters. How close the gripper should get to the hand during the handover.
@@ -50,10 +50,8 @@ soundPlayer = preloadSound_Baxter;
 
 % create the folder for file exchange
 [s,mess,messid] = mkdir(storePath);
-% if the folder already exist clear its content
-if messid == 'MATLAB:MKDIR:DirectoryExists'
-    delete([storePath,'*'])
-end
+% in case the folder already exist clear its content
+delete([storePath,'*'])
 
 %% Initialize VREP and lookup table
 
