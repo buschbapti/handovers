9e65224f60483d1fa4d2ec87f94b6d81edd3d809
buschbapti <baptiste.busch@inria.fr>
2015-12-03 17:51:06 +0100

    Optimize both arms for left handed peoples

--- src/reba_optim/read_model.py
+++ src/reba_optim/read_model.py
@@ -20,69 +20,73 @@ class ReadModel:
             self.joint_names.append('neck_'+str(i))
             self.joint_names.append('right_shoulder_'+str(i))
             self.joint_names.append('left_shoulder_'+str(i))
-            #self.joint_names.append('right_hip_'+str(i))
-            #self.joint_names.append('left_hip_'+str(i))
         # then append universal joints
         for i in range(2):
             self.joint_names.append('right_elbow_'+str(i))
             self.joint_names.append('left_elbow_'+str(i))
             self.joint_names.append('right_wrist_'+str(i))
             self.joint_names.append('left_wrist_'+str(i))
-            #self.joint_names.append('right_ankle_'+str(i))
-            #self.joint_names.append('left_ankle_'+str(i))
         # finish by the knee joint
         self.joint_names.append('right_knee')
         self.joint_names.append('left_knee')
 
     def from_model_to_transformations(self):
-        # for now create a fake model
-        self.end_effectors = ['right_hand']
-        self.chain = []
-        # create sympy variables
-        t = []
-        for i in range(self.nb_joints):
-            t.append(Symbol('t'+str(i)))
-        # get the lengths of each segmen
-        waist = self.lengths['waist_length']
-        shoulder_width = self.lengths['shoulder_offset_width']
-        shoulder_height = self.lengths['shoulder_offset_height']
-        upper_arm = self.lengths['upper_arm_length']
-        forearm = self.lengths['forearm_length']
-        hand = self.lengths['hand_length']
-        # create transformations chains
-        spine0 = translation([0,0,waist])*rotation_x(t[0])
-        spine1 = rotation_y(t[1])
-        spine2 = rotation_z(t[2])
-        shoulder0 = translation([0,-shoulder_width,shoulder_height])*rotation_z(-pi/2)*rotation_y(t[3])
-        shoulder1 = rotation_z(t[4])
-        shoulder2 = rotation_x(t[5])
-        elbow0 = translation([upper_arm,0,0])*rotation_z(t[6])
-        elbow1 = rotation_x(t[7])
-        wrist0 = translation([forearm,0,0])*rotation_z(t[8])
-        wrist1 = rotation_y(t[9])
-        hand = translation([hand,0,0])
-        # append all transformations
-        self.chain.append(spine0)
-        self.chain.append(spine1)
-        self.chain.append(spine2)
-        self.chain.append(shoulder0)
-        self.chain.append(shoulder1)
-        self.chain.append(shoulder2)
-        self.chain.append(elbow0)
-        self.chain.append(elbow1)
-        self.chain.append(wrist0)
-        self.chain.append(wrist1)
-        self.chain.append(hand)
+        def kinematic_chain(side):
+            # for now create a fake model
+            chain = []
+            # create sympy variables
+            t = []
+            for i in range(self.nb_joints):
+                t.append(Symbol('t'+str(i)))
+            # get the lengths of each segmen
+            waist = self.lengths['waist_length']
+            shoulder_width = self.lengths['shoulder_offset_width']
+            shoulder_height = self.lengths['shoulder_offset_height']
+            upper_arm = self.lengths['upper_arm_length']
+            forearm = self.lengths['forearm_length']
+            hand = self.lengths['hand_length']
+            # create transformations chains
+            spine0 = translation([0,0,waist])*rotation_x(t[0])
+            spine1 = rotation_y(t[1])
+            spine2 = rotation_z(t[2])
+            if side == 'right':
+                shoulder0 = translation([0,-shoulder_width,shoulder_height])*rotation_z(-pi/2)*rotation_y(t[3])
+            else:
+                shoulder0 = translation([0,shoulder_width,shoulder_height])*rotation_z(pi/2)*rotation_y(t[3])
+            shoulder1 = rotation_z(t[4])
+            shoulder2 = rotation_x(t[5])
+            elbow0 = translation([upper_arm,0,0])*rotation_z(t[6])
+            elbow1 = rotation_x(t[7])
+            wrist0 = translation([forearm,0,0])*rotation_z(t[8])
+            wrist1 = rotation_y(t[9])
+            hand = translation([hand,0,0])
+            # append all transformations
+            chain.append(spine0)
+            chain.append(spine1)
+            chain.append(spine2)
+            chain.append(shoulder0)
+            chain.append(shoulder1)
+            chain.append(shoulder2)
+            chain.append(elbow0)
+            chain.append(elbow1)
+            chain.append(wrist0)
+            chain.append(wrist1)
+            chain.append(hand)
+            return chain
+        # create chains for both arms
+        self.chains = []
+        self.chains.append(kinematic_chain('right'))
+        self.chains.append(kinematic_chain('left'))
 
-    def forward_kinematic(self, joints):
+    def forward_kinematic(self, joints, side=0):
         T = eye(4)
-        for i in range(len(self.chain)-1):
+        for i in range(len(self.chains[side])-1):
             # evaluate the transformation matrix with the corresponding joint value
-            temp = self.chain[i].subs('t'+str(i), joints[i]).evalf()
+            temp = self.chains[side][i].subs('t'+str(i), joints[i]).evalf()
             # postmultiply the chain
             T = T*temp
         # multiply with the transformation to the end-effector
-        T = T*self.chain[-1]
+        T = T*self.chains[side][-1]
         return T
                 
     def joint_limits(self):
--- src/reba_optim/reba_optimization.py
+++ src/reba_optim/reba_optimization.py
@@ -14,18 +14,22 @@ class RebaOptimization:
             self.cost_factors = cost_factors
         self.orientation = orientation
         self.safety_dist = safety_dist
-        self.active_joints = ['spine_0', 'spine_1', 'spine_2',
+        self.active_joints = [['spine_0', 'spine_1', 'spine_2',
                                 'right_shoulder_0', 'right_shoulder_1', 'right_shoulder_2',
                                 'right_elbow_0', 'right_elbow_1',
-                                'right_wrist_0', 'right_wrist_1']
+                                'right_wrist_0', 'right_wrist_1'],
+                                ['spine_0', 'spine_1', 'spine_2',
+                                'left_shoulder_0', 'left_shoulder_1', 'left_shoulder_2',
+                                'left_elbow_0', 'left_elbow_1',
+                                'left_wrist_0', 'left_wrist_1']]
         # initialize human model
         self.model = ReadModel()
         # initialize REBA technique
         self.reba = RebaAssess(save_score=save_score, sum_optim=sum_optim)
 
-    def get_active_joints_value(self, joints):
+    def get_active_joints_value(self, joints, side=0):
         joint_values = []
-        for name in self.active_joints:
+        for name in self.active_joints[side]:
             joint_values.append(joints[self.model.joint_names.index(name)])
         return joint_values
 
@@ -39,11 +43,11 @@ class RebaOptimization:
                 cost += abs(p - self.safety_dist[i][1])
         return cost
 
-    def cost_function(self, q):
+    def cost_function(self, q, side=0):
         # first get the active joints
-        active = self.get_active_joints_value(q)
+        active = self.get_active_joints_value(q, side)
         # calculate the forward kinematic
-        T = self.model.forward_kinematic(active)
+        T = self.model.forward_kinematic(active, side)
         # calculate the cost based on safety distance
         C_safe = self.safety_cost(T)
         # calculate cost based on desired orientation
@@ -55,8 +59,8 @@ class RebaOptimization:
         # return the final score
         return self.cost_factors[0]*C_reba + self.cost_factors[1]*C_safe + self.cost_factors[2]*C_rot
 
-    def optimize_posture(self, joints, var=0.1):
+    def optimize_posture(self, joints, side=0, var=0.1):
         joint_limits = self.model.joint_limits()['limits']
         # call optimization from scipy
-        res = minimize(self.cost_function, joints, method='L-BFGS-B', bounds=joint_limits, options={'eps':var})
+        res = minimize(self.cost_function, joints, args=(side, ), method='L-BFGS-B', bounds=joint_limits, options={'eps':var})
         return res
