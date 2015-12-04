33d27a2a1a22337e81571a56703c2b46fe83abb9
buschbapti <baptiste.busch@inria.fr>
2015-12-04 14:56:04 +0100

    Passes the hand choice through chain messages

--- scripts/test_action_client.py
+++ scripts/test_action_client.py
@@ -3,6 +3,7 @@
 import rospy
 import actionlib
 import os
+import sys
 
 from reba_optim.msg import RebaHandOverGoal, RebaHandOverAction
 
@@ -17,6 +18,7 @@ if __name__ == '__main__':
 
     goal = RebaHandOverGoal()
     goal.object = '/toolbox/side_right'
+    goal.prefered_hand_to_handover = int(sys.argv[1])
     client.send_goal(goal)
 
     #client_l.wait_for_result(rospy.Duration.from_sec(100.0))
